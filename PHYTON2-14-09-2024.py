60bb110e13ce4988e1becdff661c313b79538677
ColehoAraujo <163030717+ColehoAraujo@users.noreply.github.com>
2024-09-14 05:03:55 -0300

    Update PHYTON2-14-09-2024.py

--- PHYTON2-14-09-2024.py
+++ PHYTON2-14-09-2024.py
@@ -2,11 +2,11 @@
 sol = input("Você tem sol? (sim/não): ")
 money = input("Você tem dinheiro? (sim/não): ")
 
-# Determina o papel com base nas respostas, usando 'or' para verificar se pelo menos um é 'sim'
+# Determina as respostas, usando 'or' para verificar se pelo menos um é 'sim'
 if sol == "sim" or money == "sim":
     role = "Praia"
 else:
     role = "Netflix"
 
-# Exibe o resultado
+# Printa o resultado
 print("O seu rolê é:", role)
